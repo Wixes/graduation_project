2df279e15abd45176cba116d2777c7788642a888
Wixes <dmitri.koma@yandex.ru>
2020-02-25 21:44:06 +0700

    change some things

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const url = "mongodb://127.0.0.1:27017/usersCredentials";
 
-mongoose.connect(process.env.MONGODB_URI || url, {useNewUrlParser:true});
+mongoose.Promisa = global.Promise;
+mongoose.connect(process.env.MONGODB_URI || url, {useNewUrlParser:true, useMongoClient: true});
 
 mongoose.connection.on('connected', () => {
     console.log('Mongoose connected!');
