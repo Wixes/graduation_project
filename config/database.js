472df012d789586437341285a88a4de0ff34b0c6
Wixes <dmitri.koma@yandex.ru>
2020-02-25 21:35:11 +0700

    connection with mongodb

--- config/database.js
+++ config/database.js
@@ -1,13 +1,10 @@
 const mongoose = require('mongoose');
-const url = "mongodb+srv://Wixes98:kSynQUuVWJqRtv6@graduation-klcor.mongodb.net/test?retryWrites=true&w=majority";
+const url = "mongodb://127.0.0.1:27017/usersCredentials";
 
-mongoose.connect(url, {useNewUrlParser:true}, function (err) {
-    if (err) {
-        console.log('Error in connection to the user database...');
-    }
-    else {
-        console.log('Connected to the user database');
-    }
+mongoose.connect(process.env.MONGODB_URI || url, {useNewUrlParser:true});
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose connected!');
 });
 
 module.exports = mongoose;
