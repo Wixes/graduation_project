8e40a6816b1421005483d25e1dd82e9fd11e1959
Wixes <dmitri.koma@yandex.ru>
2020-02-28 13:27:49 +0700

    Refactor database

--- config/database.js
+++ config/database.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const url = "mongodb://127.0.0.1:27017/usersCredentials";
 
 const connectDB = async () => {
-    await mongoose.connect(process.env.MONGODB_URI ||url, {useNewUrlParser:true}, function (err) {
+    await mongoose.connect(process.env.MONGODB_URI ||url, {
+        useNewUrlParser:true,
+        useUnifiedTopology: true}, function (err) {
     if (err) {
         console.log('Error in connection to the user database...');
     }
