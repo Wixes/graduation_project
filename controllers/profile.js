b429bcf5ce640e2b0fcee12a15ce5a9cb468f51b
Wixes <dmitri.koma@yandex.ru>
2020-03-08 01:58:42 +0700

    Fixed issue with non-existent user picture in filesystem

--- controllers/profile.js
+++ controllers/profile.js
@@ -6,24 +6,37 @@ module.exports = {
     profile: async (req, res, next) => {
         console.log(req.user);
 
-        // Read file from filesystem -> encode it to base64 -> assign to variable
-        const avatar = base64pic(fs.readFileSync(req.user.picture));
-
-        res.render('profile', {
-            isAuthorised: true,
-            title: `${req.user.firstname} profile`,
-            name: req.user.firstname,
-            avatar: avatar
+        // Kludge, sorry for that :)
+        // If user previously uploaded picture, but for some reason
+        // there is no picture in filesystem
+        // default picture Will be rendered
+        fs.readFile(req.user.picture, function(err, data) {
+            let avatar = '';
+            // If file doesn't exist, then encode 'avatar' with default image
+            if (err) {
+                avatar = base64pic(fs.readFileSync('uploads/avatars/default.jpg'));
+            } else {
+                avatar = base64pic(data);
+            };
+            res.render('profile', {
+                isAuthorised: true,
+                title: `${req.user.firstname} profile`,
+                name: req.user.firstname,
+                avatar: avatar
+            });
         });
     },
 
     upload: async (req, res, next) => {
+        // Create variable for new image
         const newImage = {
             picture: req.file.path
         };
 
+        // Encode image to base64 (needed for correct re-render)
         const avatar = base64pic(fs.readFileSync(newImage.picture));
 
+        // Find user and upload an image
         await User.findByIdAndUpdate(req.user._id, newImage, function (err, res) {
             if (err) {
                 console.log ('Whoops! Something went wrong!');
@@ -32,6 +45,7 @@ module.exports = {
                 console.log('Image updated');
             }
         });
+        
         res.render('profile', {
             isAuthorised: true,
             title: `${req.user.firstname} profile`,
