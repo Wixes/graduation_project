60454ab5418b658a2c791a502fc94bb4f21c3cd5
Wixes <dmitri.koma@yandex.ru>
2020-03-27 18:14:32 +0700

    Added 9th column. Fixed table hover effect

--- public/javascript/filter_search.js
+++ public/javascript/filter_search.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+/* window.onload = function() {
     let filter = () => {
         // Element for input value
         let searchMark = document.getElementById('search-mark');
@@ -38,4 +38,77 @@ window.onload = function() {
     }
     
     filter();
+} */
+
+window.onload = function() {
+    let filter = () => {
+        // Search input for each field (mark. subject, place, date)
+        let searchMark = document.getElementById('search-mark');
+        let searchSubject = document.getElementById('search-subject');
+        let searchPlace = document.getElementById('search-place');
+        let searchDate = document.getElementById('search-date');
+
+        // All items with data-item attribute
+        let dataItems = document.querySelectorAll('[data-item]');
+        console.log('just row items: ', dataItems);
+
+        // Array for storing elements in the order (1st elem =  all elements with [data-item = '1'] etc...)
+        let itemsArray = [];
+        // Assign each element to array
+        dataItems.forEach(item => {
+            let attr = item.getAttribute('data-item');
+            if (itemsArray[attr] == undefined)
+                itemsArray[attr] = [];
+            itemsArray[attr].push(item);
+        });
+        console.log('array: ', itemsArray);
+        console.log('test elem: ', itemsArray[0][1]);
+
+        // Add event 'keyup' for search mark
+        searchMark.addEventListener('keyup', function() {
+            console.log('key is pressed');
+            searchHandler('.item-file-mark', itemsArray);
+        });
+
+        // Add event 'keyup' for search subject
+        searchSubject.addEventListener('keyup', function() {
+            console.log('key is pressed');
+            searchHandler('.item-file-subject', itemsArray);
+        });
+
+        // Add event 'keyup' for search place
+        searchPlace.addEventListener('keyup', function() {
+            console.log('key is pressed');
+            searchHandler('.item-file-place', itemsArray);
+        });
+
+        // Add event 'keyup' for search date
+        searchDate.addEventListener('keyup', function() {
+            console.log('key is pressed');
+            searchHandler('.item-file-date', itemsArray);
+        });
+    };
+
+    filter();
+
+    // Function to handle search
+    let searchHandler = (itemClass, itemsArray) => {
+        console.log('itemsArray from function:', itemsArray);
+        let items = document.querySelectorAll(itemClass);
+        console.log('items from function: ', items);
+        let newItem;
+        
+        // Create new array with the same elements from different data-item
+        let sortItems = itemsArray.map(el => {
+            console.log('element: ', el);
+            for (const elem of el) {
+                if(elem.matches(itemClass))
+                    newItem = elem;
+            }
+            return newItem;
+        });
+        
+        console.log('Array with elements from different data-items: ', sortItems);
+
+    };
 }
